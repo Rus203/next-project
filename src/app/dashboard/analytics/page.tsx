064bf2b8f3c1c6bae0d626cc180a05224aa91d04
Ruslan <c8man@gmail.com>
2023-07-24 18:33:36 +0200

    init: start the project

---
+++ src/app/dashboard/analytics/page.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const Analytics: FC = () => {
+  return <div>Analytics</div>;
+};
+
+export default Analytics;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import Link from 'next/link';
+import React, { FC } from 'react';
+import style from './navbar.module.css';
+
+const links = [
+  {
+    id: 1,
+    title: 'Home',
+    url: '/'
+  },
+  {
+    id: 2,
+    title: 'Portfolio',
+    url: '/portfolio'
+  },
+  {
+    id: 3,
+    title: 'Blog',
+    url: '/blog'
+  },
+  {
+    id: 4,
+    title: 'about',
+    url: '/about'
+  },
+  {
+    id: 5,
+    title: 'contact',
+    url: '/contact'
+  },
+  {
+    id: 6,
+    title: 'dashboard',
+    url: '/dashboard'
+  }
+];
+
+const Navbar: FC = () => {
+  return (
+  <div className={style.container} >
+    <Link className={style.logo} href='/'>Rus203</Link>
+    <div className={style.links}>
+    { links.map(page => 
+      (<Link href={page.url} key={page.id}>{page.title}</Link>))}
+    <button className={style.logout}
+      onClick={() => {
+        console.log('logout');
+        }}>
+          Log out
+      </button>
+    </div>
+  </div>);
+};
+
+export default Navbar;
