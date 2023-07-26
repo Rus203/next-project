cf6c40b5b30649bb385dfd6f99284023925b071e
Ruslan <c8man@gmail.com>
2023-07-26 15:45:08 +0200

    feat: add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,38 @@
 import React, { FC } from 'react';
+import style from './contact.module.css';
+import Image from 'next/image';
+import { Button } from '@/components/Button';
 
 const Contact: FC = () => {
-  return <div>Contact</div>;
+  return <div className={style.container}>
+    <h2 className={style.title} >Let&#39;s Keep in touch</h2>
+    <div className={style.content}>
+      <div className={style.imgContainer}>
+        <Image
+          src='/contact.png'
+          fill={true}
+          className={style.img}
+          alt='picture'
+        />
+      </div>
+      <form className={style.form}>
+        <input type="text"
+          placeholder='name'
+          className={style.input}/>
+        <input
+          placeholder='email'
+          type="email"
+          className={style.input}/>
+        <textarea
+          className={style.textArea}
+          placeholder='message'
+          rows={10}
+          cols={30}
+        ></textarea>
+        <Button text="Send" url="#" />
+      </form>
+    </div>
+  </div>;
 };
 
 export default Contact;
