18dfc4c6dec2a57a2b869c70d6cd9a5486b1c634
Ruslan <c8man@gmail.com>
2023-07-26 15:44:50 +0200

    feat: add blog

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,7 +1,65 @@
 import React, { FC } from 'react';
+import style from './page.module.css';
+import Image from 'next/image';
 
-const BlogPost: FC = () => {
-  return <div>Blog</div>;
+interface IProps {
+  params: {
+    id: string;
+  }
+}
+
+const BlogPost: FC<IProps> = ({ params }) => {
+  const { id } = params;
+  console.log(id);
+  return (
+    <div className={style.container}>
+      <div className={style.top}>
+        <div className={style.info}>
+          <h2 className={style.title}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </h2>
+          <p className={style.desciption}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            vel est quisquam perferendis blanditiis totam ducimus, cupiditate id
+            soluta illo eius optio inventore quos autem, quaerat neque?
+          </p>
+          <div className={style.author}>
+            <Image
+              src ='https://images.pexels.com/photos/3772623/pexels-photo-3772623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              alt='avatar'
+              className={style.avatar}
+              width={40}
+              height={40}
+            />
+            <span className={style.user}>John Doe</span>
+          </div>
+        </div>
+        <div className={style.imgContainer}>
+          <Image
+            src='https://images.pexels.com/photos/2876407/pexels-photo-2876407.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt='image'
+            className={style.img}
+            fill={true}
+          />
+        </div>
+      </div>
+      <div className={style.content}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
+        dolorum rem earum consequatur similique eveniet cum sit expedita.
+        Voluptate pariatur quos, ut quidem ipsam voluptatibus, ad quae
+        eligendi vitae perspiciatis maxime, est expedita! Esse vitae
+        vero possimus doloribus sequi corporis pariatur, reiciendis
+        fugiat modi itaque, iste quaerat, voluptate odio aperiam
+        perferendis totam. Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Voluptate dolorum rem earum consequatur similique
+        eveniet cum sit expedita. Voluptate pariatur quos, ut quidem
+        ipsam voluptatibus, ad quae eligendi vitae perspiciatis maxime,
+        est expedita! Esse vitae vero possimus doloribus sequi corporis
+        pariatur, reiciendis fugiat modi itaque, iste quaerat, voluptate
+        odio aperiam  perferendis totam.
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,53 @@
 import React, { FC } from 'react';
+import style from './blog.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Blog: FC = () => {
-  return <div>Blog</div>;;
+  return (
+    <div className={style.container}>
+      <Link href="#" className={style.post}>
+        <div className={style.imgContainer}>
+          <Image
+            src='https://images.pexels.com/photos/13911606/pexels-photo-13911606.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt='post'
+            className={style.img}
+            fill={true}
+          />
+        </div>
+        <div className={style.textContainer}>
+          <h3 className={style.title}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, unde.</h3>
+          <p className={style.description}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+            cum dolorem ipsum enim praesentium dolor illum officia a, amet
+            possimus adipisci, laudantium tenetur earum repellat ratione!
+            Atque eum illo dolore.
+          </p>
+        </div>
+      </Link>
+
+      <Link href="#" className={style.post}>
+        <div className={style.imgContainer}>
+          <Image
+            src='https://images.pexels.com/photos/13911606/pexels-photo-13911606.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt='post'
+            className={style.img}
+            fill={true}
+          />
+        </div>
+        <div className={style.textContainer}>
+          <h3 className={style.title}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, unde.</h3>
+          <p className={style.description}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+            cum dolorem ipsum enim praesentium dolor illum officia a, amet
+            possimus adipisci, laudantium tenetur earum repellat ratione!
+            Atque eum illo dolore.
+          </p>
+        </div>
+      </Link>
+
+      
+    </div>);
 };
 
 export default Blog;
